41cd9496b46be0a91516d5b3b7d49cbb5d223079
Juan Ángel Espigares Marín <juanangel.espigares@imaginanet.com>
2019-07-27 11:24:39 +0200

    Trying to add thumbnail buttons in Windows, not working :(

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,6 @@
 
 import { app, BrowserWindow, globalShortcut } from 'electron'
 import windowStateKeeper from 'electron-window-state'
-import path from 'path'
 import createMenu from './createMenu'
 import registerGlobalShortcuts from './registerGlobalShortcuts'
 import listenToEvents from './listenToEvents'
@@ -32,6 +31,9 @@ const createWindow = () => {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
     x: mainWindowState.x,
     y: mainWindowState.y,
     width: mainWindowState.width < MIN_WINDOW_WIDTH ? MIN_WINDOW_WIDTH : mainWindowState.width,
@@ -44,11 +46,19 @@ const createWindow = () => {
     darkTheme: true,
     show: false,
     backgroundColor: '#181818',
-    frame: false,
-    icon: path.join(__dirname, '/src/renderer/img/logo.png')
+    frame: false
   })
 
-  mainWindow.once('ready-to-show', () => mainWindow.show())
+  // const test = nativeImage.createFromPath("/test.png")
+  // //imgs/bars--img.gif
+  // console.log(test.isEmpty())
+  // console.log(test.getSize())
+  // console.log(test.toDataURL())
+  // const test = nativeImage.createFromPath('/test.png')
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 
   if (mainWindowState.isMaximized) {
     mainWindow.maximize()
@@ -58,29 +68,67 @@ const createWindow = () => {
 
   mainWindow.loadURL(winURL)
 
+  // mainWindow.setThumbarButtons([{
+  //   tooltip: 'Example',
+  //   icon: nativeImage.createFromPath('/test.png'),
+  //   // icon: nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAB3RJTUUH4wcbCAYYIhmhagAAAXFJREFUWMPF1zFIlVEYxvH3XFHppinS0FKI4BRIEDS25SK4tbnVFm05B0Iuji5ONecS0dQQTdHUJDQIDkGIkCQNZVj6c/FDucjnd6/f+e5/OOv7H95znudEnIDH2MYH3I8mQcKeU47wGreaEhg4GdrJbzxHO7fAkHK+4SFSLoErqvERd3IItFXnH9ZwvU6Bq7rnJ55isA6BEb2zgQf9FCh4g6leBUbVwz6WMdKtwDX18h0LaPVLoOAT7vVTAA7xCjfKBMbk5xeeYeg8gXHN8RV3i9mpEIiIvQYDeCcibqaU/reiPwxHRCuKo2G2ImIupXRwViA1MHg/IpYiYial9LlzCScyLt0R1jFZdg1zCXyp1C8zCOzgEQaqPsV1CfzFCsa6DaM6BN5iutc4vsxLuIHZyxaSXgR28aSuStaNwAFWMVFnKa0ax+9xO0ctv6iSbWI+58+oXZLhixjO/TVr4U9Hi3lZ2mIySLzAD7w7Wxhycwz23/4IZqBmOAAAAABJRU5ErkJggg=='),
+  //   flags: ['enabled', 'dismissonclick'],
+  //   click: function () {
+  //     console.log('Example button clicked')
+  //   }
+  // }, {
+  //   tooltip: 'Example 2',
+  //   icon: nativeImage.createFromPath('/test.png'),
+  //   // icon: nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAB3RJTUUH4wcbCAYYIhmhagAAAXFJREFUWMPF1zFIlVEYxvH3XFHppinS0FKI4BRIEDS25SK4tbnVFm05B0Iuji5ONecS0dQQTdHUJDQIDkGIkCQNZVj6c/FDucjnd6/f+e5/OOv7H95znudEnIDH2MYH3I8mQcKeU47wGreaEhg4GdrJbzxHO7fAkHK+4SFSLoErqvERd3IItFXnH9ZwvU6Bq7rnJ55isA6BEb2zgQf9FCh4g6leBUbVwz6WMdKtwDX18h0LaPVLoOAT7vVTAA7xCjfKBMbk5xeeYeg8gXHN8RV3i9mpEIiIvQYDeCcibqaU/reiPwxHRCuKo2G2ImIupXRwViA1MHg/IpYiYial9LlzCScyLt0R1jFZdg1zCXyp1C8zCOzgEQaqPsV1CfzFCsa6DaM6BN5iutc4vsxLuIHZyxaSXgR28aSuStaNwAFWMVFnKa0ax+9xO0ctv6iSbWI+58+oXZLhixjO/TVr4U9Hi3lZ2mIySLzAD7w7Wxhycwz23/4IZqBmOAAAAABJRU5ErkJggg=='),
+  //   flags: ['enabled', 'dismissonclick'],
+  //   click: function () {
+  //     console.log('Example button clicked')
+  //   }
+  // }, {
+  //   tooltip: 'Example 3',
+  //   icon: nativeImage.createFromPath('/test.png'),
+  //   // icon: nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAB3RJTUUH4wcbCAYYIhmhagAAAXFJREFUWMPF1zFIlVEYxvH3XFHppinS0FKI4BRIEDS25SK4tbnVFm05B0Iuji5ONecS0dQQTdHUJDQIDkGIkCQNZVj6c/FDucjnd6/f+e5/OOv7H95znudEnIDH2MYH3I8mQcKeU47wGreaEhg4GdrJbzxHO7fAkHK+4SFSLoErqvERd3IItFXnH9ZwvU6Bq7rnJ55isA6BEb2zgQf9FCh4g6leBUbVwz6WMdKtwDX18h0LaPVLoOAT7vVTAA7xCjfKBMbk5xeeYeg8gXHN8RV3i9mpEIiIvQYDeCcibqaU/reiPwxHRCuKo2G2ImIupXRwViA1MHg/IpYiYial9LlzCScyLt0R1jFZdg1zCXyp1C8zCOzgEQaqPsV1CfzFCsa6DaM6BN5iutc4vsxLuIHZyxaSXgR28aSuStaNwAFWMVFnKa0ax+9xO0ctv6iSbWI+58+oXZLhixjO/TVr4U9Hi3lZ2mIySLzAD7w7Wxhycwz23/4IZqBmOAAAAABJRU5ErkJggg=='),
+  //   flags: ['enabled', 'dismissonclick'],
+  //   click: function () {
+  //     console.log('Example button clicked')
+  //   }
+  // }])
+  // mainWindow.setOverlayIcon(test, 'TEst asdf')
+  // mainWindow.setProgressBar(0.5)
+
   // This is done this way because I don't want to change the renderer folder
   mainWindow.webContents.on('dom-ready', () => {
     mainWindow.webContents.executeJavaScript(`
-      const {remote} = require('electron')
+      const path = require('path')
+      const { remote } = require('electron')
+      const win = remote.getCurrentWindow()
       const ElectronTitlebarWindows = require('electron-titlebar-windows')
       const titleBar = new ElectronTitlebarWindows({
         backgroundColor: '#181818',
         draggable: true,
       })
+      // Set the thumbnail region (windows)
+      win.setThumbnailClip({
+        x: 256, y: window.innerHeight - 63, width: 64, height: 63
+      })
+      window.addEventListener('resize', function(e) {
+        win.setThumbnailClip({
+          x: 256, y: window.innerHeight - 63, width: 64, height: 63
+        })
+      })
+      
       titleBar.on('minimize', function(e) {
-        remote.getCurrentWindow().minimize();
-      });
+        win.minimize();
+      })
       titleBar.on('maximize', function(e) {
-        let win = remote.getCurrentWindow();
         win.isMaximized() ? win.unmaximize() : win.maximize();
-      });
+      })
       titleBar.on('fullscreen', function(e) {
-        let win = remote.getCurrentWindow();
         win.isMaximized() ? win.unmaximize() : win.maximize();
-      });
+      })
       titleBar.on('close', function(e) {
-          remote.getCurrentWindow().close();
-      });
+          win.close();
+      })
       
       // Put the title bar in the correct position
       titleBar.appendTo(document.body)
